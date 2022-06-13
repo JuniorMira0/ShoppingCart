36b16d641160059f16651367d939ee456f54c197
Elithon Silva <elithonsilva@gmail.com>
2022-06-13 19:05:08 -0300

    rename ProductContent.jsx > ProductContainer.jsx e importa em Home.jsx

---
+++ src/components/ProductContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProductContainer() {
+  return (
+    <div className="content-card">
+      <p>container-card</p>
+    </div>
+  );
+}
+
+export default ProductContainer;
