84bdc6e4263ec4a5c695e5a09c407391fd745e9c
Elithon Silva <elithonsilva@gmail.com>
2022-06-13 18:14:51 -0300

    install eslint

--- src/routes/Switcher.jsx
+++ src/routes/Switcher.jsx
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Checkout from '../pages/Checkout';
 import NotFound from '../pages/NotFound';
 
-class Switcher extends Component {
-  render() {
-    return (
-      <Routes>
-        <Route path="/checkout" element={ <Checkout/> } />
-        <Route exact path="/" element={ <Home/> } />
-        <Route exact path="*" element={ <NotFound/> } />
-      </Routes>
-    );
-  }
+function Switcher() {
+  return (
+    <Routes>
+      <Route path="/checkout" element={<Checkout />} />
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
 
 export default Switcher;
