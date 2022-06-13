df80af222becf662cb7573c3afaf515a5fbb3fdc
junior <j-unyor14@hotmail.com>
2022-06-13 19:17:55 -0300

    fixing header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../components/Header';
 
 function Home() {
   return (
     <div className="Home">
-      <p>Home Page</p>
+      <Header />
     </div>
   );
 }
